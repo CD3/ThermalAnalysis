a055209cea0cc32d6c13ee8d135d081212ff11a4
Daniel Huantes <daniel.huantesjr@gmail.com>
2020-02-11 15:41:57 -0600

    Removed storage of Field objects in place of Interpolators

--- testing/CatchTests/TempBuilder.cpp
+++ testing/CatchTests/TempBuilder.cpp
@@ -7,6 +7,8 @@
 #include <libInterpolate/Interpolate.hpp>
 using namespace std;
 
+//get type for Interpolator setup
+//
 // Writing an implementation, in plain english this implementation needs to:
 // have a function add to add (function? field?) values to some container in
 // mem, to be stored. There also needs a build method that will assign (not
@@ -18,20 +20,50 @@ using namespace std;
 // Be general, not just geometric time spacing may be used
 class LinearCombination
 {
+
  public:
+  using currentType = _1D::LinearInterpolator<double>;
+  vector<double> alphas;
+  vector<double> offsets;
+  vector<_1D::LinearInterpolator<double>> interpolators;
+
+  template <class Obj>
+  struct get_data_type {
+    using type = void;
+  };
+  template <template <typename> class Obj, typename T>
+  struct get_data_type<Obj<T>> {
+    using type = T;
+  };
+  template<typename Obj>
+  using get_data_type_t = typename get_data_type<Obj>::type;
+
+  //simple method to encapsulate making a interpolator and setting data to a
+  //field's data. Utitilizes get_data_type_t, etc
+  template <class InterpType>
+  auto MakeInterpolator(const Field<get_data_type_t<InterpType>,1>& F)
+  {
+    InterpType interp;
+    interp.setData(F.size(), F.getCoordinateSystem().getAxis(0).data(), F.data());
+    return interp;
+  }
   //3 vectors with corresponding indexing. the profiles[i] should be multiplied
   //by a scalar multiple alphas[i], and its times offsett by offsets[i]. These
   //are member variables in order to be captured 'all in one' through capturing
   //of 'this' in the lambda expression in build
-  vector<Field<double, 1>> profiles;
-  vector<double> alphas;
-  vector<double> offsets;
-  _1D::MonotonicInterpolator<double> interp;
+
+  //look into switching _1d:: (typed twice) to VVV
+  //template <typename I>
+  //_1D::MonotonicInterpolator<double> interp;
+
   
   template <typename F, typename T, typename S>
   void add(const F& field, T t, S scale)
   {
-    profiles.push_back(field);
+    //unimplemented
+    //exchange _1D for any interpolator type
+    auto interp = MakeInterpolator<_1D::LinearInterpolator<double>>(field);
+    interpolators.push_back(interp);
     offsets.push_back(t);
     alphas.push_back(scale);
   }
@@ -50,36 +82,23 @@ class LinearCombination
     //being better at interpolating
     field.set_f([this, &field](auto t) mutable{
     int tem = 0;
-    auto local_prof = (this->profiles);
     auto local_alph = (this->alphas);
     auto local_off = (this->offsets);
-    auto local_interp = (this->interp);
+    auto local_interps = (this->interpolators);
+    //currently replacing adding field with adding interpolator
     for(int i = 0; i < t.size(); i++){
+        // i is always 0, this is just a way to unpack the list while not breaking under 
         double Temp = 0;
-        for(int j = 0; j < local_prof.size(); j++){
-          auto t_data = local_prof[j].getAxis(0);
-          for(int k = 0; k < t_data.size(); k++){
-            t_data[k] += local_off[j];
-          }
-          double a = local_alph[j];
-//          Field<double, 1> l_p = local_prof[j];
-//          local_prof[j].set_f([a, l_p](auto t) {
-//            return l_p[0](t) * a;
-//          });
-//          local_interp.setData(t_data, local_prof[j].getData() * a);
-//          Temp += local_interp(t[i]);
-          local_interp.setData(t_data, local_prof[j]);
-          Temp += local_interp(t[i]) * local_alph[j];
-          //^^^^^^^ that is the old way, currently trying multiplying by alpha first
+        for(int j = 0; j < local_interps.size(); j++){
+          //offsets does not work, gnuplot starts at 0
+          Temp += (local_interps[j](t[i] - offsets[j]) * local_alph[j]);
           /*string str_loc = "Currently in:\nprofile #" + std::to_string(j);
           string str_ao  = "\nalpha = " + std::to_string(local_alph[j]) + "\noffset = " + std::to_string(local_off[j]);
           string str_interp = "\nraw interp = " + std::to_string(local_interp(t[i]));
           string str_adj = "\nadj interp = " + std::to_string(local_interp(t[i]) * local_alph[j]);
           string sout = str_loc + str_ao + str_interp + str_adj + "\n\n------------------------------------\n ";
         std::cout << sout;*/
-        }
-      return Temp;
-    }
+        } return Temp; }
     });
     return;
   }
