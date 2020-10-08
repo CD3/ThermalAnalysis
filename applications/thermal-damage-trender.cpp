67c6b9d6bb6eac8de82525d7cf0ecd2b0ca4a9cd
CD Clark III <clifton.clark@gmail.com>
2020-10-08 11:54:57 -0500

    feat: added command line options to configure the thermal damage trender.

--- applications/thermal-damage-trender.cpp
+++ applications/thermal-damage-trender.cpp
@@ -35,10 +35,16 @@ int main(int argc, const char** argv)
   po::options_description po_opts("Options");
   po_opts.add_options()("help,h", "print help message.")(
       "verbose,v", po::value<int>()->implicit_value(0), "verbose level.")
-      ("Ea", po::value<double>()->default_value(6.28e5), "Activation energy.")
-      ("A",  po::value<double>()->default_value(3.1e99), "Frequency factor.")
-      ("write-profiles", "Write damage threshold profiles.")
-      ;
+    ("Ea", po::value<double>()->default_value(6.28e5), "Activation energy.")
+    ("A",  po::value<double>()->default_value(3.1e99), "Frequency factor.")
+    ("temperature-bias",  po::value<double>()->default_value(0.0), "A temperature offset that will be added to the temperature history to convert it to absolute temperature.")
+    ("exposure",  po::value<double>()->default_value(1.0), "The exposure that gave the temperature history. I.e., if the temperature history corresponds to a 2.5 W exposure, then computed damage threshold scaling factors will be multiplied by 2.5 and the threshold values will correspond to power.")
+    ("tau-min",  po::value<double>()->default_value(10e-6), "The minimum exposure duration to compute the damage threshold for.")
+    ("tau-max",  po::value<double>(), "The maximum exposure duration to compute the damage threshold for. The default is to use the entire exposure, but this may not be accurate if the cool down period is important.")
+    ("tau-reduction-factor",  po::value<double>()->default_value(2), "The reduction factor between consecutive exposure duration. I.e., a reduction factor of 2 would cut the exposure duration in half each time.")
+    ("dt",  po::value<double>()->default_value(10e-6), "Time resolution to use for generated temperature histories.")
+    ("write-profiles", "Write damage threshold profiles.")
+    ;
 
   // now define our arguments.
   po::options_description po_args("Arguments");
@@ -71,6 +77,12 @@ int main(int argc, const char** argv)
     return 0;
   }
 
+  if(vm["tau-reduction-factor"].as<double>() <= 1)
+  {
+    std::cerr << "Error: The tau reduction factor must be greater than 1." << std::endl;
+    return 1;
+  }
+
 
   for( auto file : vm["thermal-profiles"].as<std::vector<std::string>>() )
   {
@@ -88,29 +100,32 @@ int main(int argc, const char** argv)
     }
 
 
+    Tvst += vm["temperature-bias"].as<double>();
     auto T0 = Tvst[0];
     auto tmin = Tvst.getCoord(0);
     auto tmax = Tvst.getCoord(Tvst.size()-1);
 
     Tvst -= T0;
 
-    // calculate how many 
+    // calculate how many
     std::vector<double> taus;
     taus.push_back(tmax-tmin);
-    while( taus[taus.size()-1] > 100e-6 )
+    if( vm.count("tau-max") > 0 )
     {
-      taus.push_back(taus[taus.size()-1]/2);
+      taus[0] = vm["tau-max"].as<double>();
+    }
+    while( taus[taus.size()-1] > vm["tau-min"].as<double>() )
+    {
+      taus.push_back(taus[taus.size()-1]/vm["tau-reduction-factor"].as<double>());
     }
 
     double dt = 10e-6;
-
+    int N = tmax/dt;
     libArrhenius::ThresholdCalculator< libArrhenius::ArrheniusIntegral<double> > calc(vm["A"].as<double>(),vm["Ea"].as<double>());
     for( auto tau : taus)
     {
-      double tmax2 = std::min(tmin + 2*tau,tmax);
-      int N = tmax2/dt;
       Field<double,1> Tvst2(N);
-      Tvst2.setCoordinateSystem( Uniform(tmin,tmax2) );
+      Tvst2.setCoordinateSystem( Uniform(tmin,tmax) );
       LinearCombination<_1D::MonotonicInterpolator<double>> tempBuilder;
 
       tempBuilder.add(Tvst, 0, 1.);
@@ -120,8 +135,8 @@ int main(int argc, const char** argv)
       Tvst2 += T0;
 
       double threshold = calc(Tvst2.size(), Tvst2.getAxis(0).data(),Tvst2.data() );
-      std::cout << tau << " " << threshold << std::endl;
-      
+      std::cout << tau << " " << threshold * vm["exposure"].as<double>() << std::endl;
+
       if(vm.count("write-profiles"))
       {
         std::string ofile = file+"-"+boost::lexical_cast<std::string>(tau)+"-threshold";
@@ -141,7 +156,7 @@ int main(int argc, const char** argv)
 
 
 
-    
+
 
 
 
